bd2b7c3d0504ffb63aa3e777848e92f63f7e8bd1
Rasul Jafarli <rjafarli20@gmail.com>
2021-04-06 10:54:17 +0400

    first

---
+++ assa/assa/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using assa.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assa.Data
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ assa/assa/Data/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assa.Data.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        [StringLength(maximumLength:50)]
+        public string FullName { get; set; }
+        public bool IsMember { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
